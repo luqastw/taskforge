9a2d1e8252599f7727770c9de6e5e84652f0cf2f
luqastw <lucasborcem1@gmail.com>
2026-04-03 20:46:13 -0300

    feat: configure Pest PHP as testing framework

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,13 @@
 <?php
 
-namespace Tests\Feature;
+declare(strict_types=1);
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+test('application returns a successful response', function () {
+    $response = $this->get('/');
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    $response->assertStatus(200);
+});
 
-        $response->assertStatus(200);
-    }
-}
+test('example test passes', function () {
+    expect(true)->toBeTrue();
+});
---
+++ tests/Pest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+uses(TestCase::class, RefreshDatabase::class)->in('Feature');
+uses(TestCase::class)->in('Unit');
+
+/*
+|--------------------------------------------------------------------------
+| Expectations
+|--------------------------------------------------------------------------
+*/
+
+expect()->extend('toBeOne', function () {
+    return $this->toBe(1);
+});
+
+/*
+|--------------------------------------------------------------------------
+| Functions
+|--------------------------------------------------------------------------
+*/
+
+function something(): string
+{
+    return 'something';
+}
